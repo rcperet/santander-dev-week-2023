3878098c10c9caafd3aa583bbb8d44b6e602669d
[EXT] - RAPHAEL DA COSTA PERET <rperet@brasilseg.com.br>
2025-05-12 21:07:06 -0300

    Implementando o projeto

---
+++ src/main/java/me/dio/domain/model/Account.java
@@ -0,0 +1,33 @@
+package me.dio.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_account")
+public class Account {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(unique = true)
+	private String number;
+	
+	
+	private String agency;
+	
+	@Column(precision = 13, scale = 2)
+	private BigDecimal balance;
+	 
+	 @Column(name = "additional_limit", precision = 13, scale = 2)
+	private BigDecimal limit;
+	
+	
+}
---
+++ src/main/java/me/dio/domain/model/Card.java
@@ -0,0 +1,26 @@
+package me.dio.domain.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_card")
+public class Card {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String number;
+
+    @Column(name = "available_limit", precision = 13, scale = 2)
+    private BigDecimal limit;
+
+}
